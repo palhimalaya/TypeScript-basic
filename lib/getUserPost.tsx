5ae859544b768b1d83400cac6e12c7b25ba55bc6
palhimalaya <himalayapal123@gmail.com>
2023-04-03 23:01:36 +0545

    getting users post from api

---
+++ lib/getUserPost.tsx
@@ -0,0 +1,10 @@
+const getUserPost = async (userId: string) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?userId=${userId}`,
+    { next: { revalidate: 10 } }
+  );
+  if (!res.ok) return undefined;
+  return res.json();
+};
+
+export default getUserPost;
