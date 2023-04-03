c8e49323e3adbd1337325891f873ca8774f74ffc
palhimalaya <himalayapal123@gmail.com>
2023-04-03 22:59:55 +0545

    generating Static Params

---
+++ app/users/[userId]/page.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import getUser from "@/lib/getUser";
+import getUserPost from "@/lib/getUserPost";
+import { Suspense } from "react";
+import UserPosts from "./components/UserPosts";
+import type { Metadata } from "next";
+import NotFound from "./not-found";
+import getAllUsers from "@/lib/getAllUsers";
+
+type Params = {
+  params: {
+    userId: string;
+  };
+};
+
+export async function generateMetadata({
+  params: { userId },
+}: Params): Promise<Metadata> {
+  const userData: Promise<User> = getUser(userId);
+  const user: User = await userData;
+
+  if (!user.name) {
+    return {
+      title: "User Not Found",
+    };
+  }
+
+  return {
+    title: user.name,
+    description: `This is the page of ${user.name}`,
+  };
+}
+
+export default async function UserPage({ params: { userId } }: Params) {
+  const userData: Promise<User> = getUser(userId);
+  const userPostsData: Promise<Post[]> = getUserPost(userId);
+
+  //   const [user, userPosts] = await Promise.all([userData, userPostsData]);
+  const user = await userData;
+
+  if (!user.name) return NotFound();
+
+  return (
+    <>
+      <h2>{user.name}</h2>
+      <br />
+      <Suspense fallback={<h2>Loading.....</h2>}>
+        {/* @ts-expect-error Server Component */}
+        <UserPosts promise={userPostsData} />
+      </Suspense>
+    </>
+  );
+}
+
+export async function generateStaticParams() {
+  const usersData: Promise<User[]> = getAllUsers();
+  const users = await usersData;
+
+  return users.map((user) => ({
+    userId: user.id.toString(),
+  }));
+}
